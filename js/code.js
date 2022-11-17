55373f9c12969c46a9f539625835537a292b522f
Likay01 <mgoqilindokuhle2gmail.com>
2022-11-17 12:36:36 -0800

    Update

--- js/code.js
+++ js/code.js
@@ -1,4 +1,4 @@
 function myFunction() {
     let text = document.getElementById("book").innerHTML;
-    document.getElementById("book").innerHTML = text.replace("Waiting on Check in...","You are checked in! " );
+    document.getElementById("book").innerHTML = text.replace("Waiting on Check in...","✔ You are checked in! " );
 }
